696363a6d9fe9e3c8724f43366c749850b6af713
BuildTools <unconfigured@null.spigotmc.org>
2020-08-30 12:05:15 +0200

    Initial commit

---
+++ src/main/java/me/daqem/spigothelp/Spigothelp.java
@@ -0,0 +1,17 @@
+package me.daqem.spigothelp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spigothelp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
